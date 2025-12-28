b3eea6478e80e3595cce9c151fab36745200031a
Ryan <rzegalia@cobrafiringsystems.com>
2025-12-28 09:50:11 -0500

    Change port to 3030 for MADSERVER deployment

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: 3001
+    port: 3030
   },
   preview: {
     host: '0.0.0.0',
-    port: 3001
+    port: 3030
   }
 })
